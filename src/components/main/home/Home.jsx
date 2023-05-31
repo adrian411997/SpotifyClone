9c3461dbeb5e8f8537662563e95f1988677df65c
adrian411997 <adrian_mart@hotmail.com.ar>
2023-05-31 18:20:12 -0300

    SideRightComponentes

--- src/components/main/home/Home.jsx
+++ src/components/main/home/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import SideLeft from "../SideLeft/SideLeft";
+import SideRigth from "../SideRigth/SideRigth";
+import "./Home.css";
 
 const Home = () => {
-  return <SideLeft />;
+  return (
+    <div className="space-work">
+      <SideLeft />
+      <SideRigth />
+    </div>
+  );
 };
 
 export default Home;
